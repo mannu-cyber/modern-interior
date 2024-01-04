d52008527e4a1c607562c2911085e5b7ca45bc5f
Abhimanyu sahani <sahanimannu844@gmail.com>
2024-01-04 13:42:43 +0530

    removed debug

--- script.js
+++ script.js
@@ -1,44 +1,44 @@
-// const scroll = new LocomotiveScroll({
-//   el: document.querySelector(".main"),
-//   smooth: true,
-// });
+const scroll = new LocomotiveScroll({
+  el: document.querySelector(".main"),
+  smooth: true,
+});
 
 
 
-  // Shery.mouseFollower( {
+  Shery.mouseFollower( {
 
-  //   skew: true,
-  //   ease: "cubic-bezier(0.23, 1, 0.320, 1)",
-  //   duration: 0.5,
-  // });
-
-
-
-
-
-
-// let tl = gsap.timeline()
-
-// tl.from(".nav a, .navb",{
-// y:20,
-// // duration:.3,
-// stagger:.5,
-// opacity:0,
-// scale:0,
-
-// })
+    skew: true,
+    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+    duration: 0.5,
+  });
 
 
 
 
-// Shery.imageEffect(".right-content img", {
-//   style: 7, //Select Style
-//   debug: true, // Debug Panel
-//   config: {
-//     /* Config made from debug panel */
-//   },
-//   preset: "./presets/wigglewobble.json",
-// });
+
+
+let tl = gsap.timeline()
+
+tl.from(".nav a, .navb",{
+y:20,
+// duration:.3,
+stagger:.5,
+opacity:0,
+scale:0,
+
+})
+
+
+
+
+Shery.imageEffect(".right-content img", {
+  style: 7, //Select Style
+  // debug: true, // Debug Panel
+  config: {
+    /* Config made from debug panel */
+  },
+  preset: "./presets/wigglewobble.json",
+});
 
 
 
@@ -63,7 +63,7 @@ let imageEffect = document.querySelector(".page4 img")
 imageEffect.addEventListener("mouseover", function(){
   Shery.imageEffect(".page4 img", {
     style: 6, //Select Style
-    debug: true, // Debug Panel
+    // debug: true, // Debug Panel
 
     gooey:true,
     config:  {"noiseDetail":{"value":7.44,"range":[0,100]},"distortionAmount":{"value":2.98,"range":[0,10]},"scale":{"value":36.36,"range":[0,100]},"speed":{"value":0.79,"range":[0,1]},"zindex":{"value":-9996999,"range":[-9999999,9999999]},"aspect":{"value":0.6847925639670689},"ignoreShapeAspect":{"value":true},"shapePosition":{"value":{"x":0,"y":0}},"shapeScale":{"value":{"x":0.5,"y":0.5}},"shapeEdgeSoftness":{"value":0,"range":[0,0.5]},"shapeRadius":{"value":0,"range":[0,2]},"currentScroll":{"value":0},"scrollLerp":{"value":0.07},"gooey":{"value":true},"infiniteGooey":{"value":false},"growSize":{"value":4,"range":[1,15]},"durationOut":{"value":1,"range":[0.1,5]},"durationIn":{"value":1.5,"range":[0.1,5]},"displaceAmount":{"value":0.5},"masker":{"value":false},"maskVal":{"value":1,"range":[1,5]},"scrollType":{"value":0},"geoVertex":{"range":[1,64],"value":1},"noEffectGooey":{"value":true},"onMouse":{"value":0},"noise_speed":{"value":0.2,"range":[0,10]},"metaball":{"value":0.2,"range":[0,2]},"discard_threshold":{"value":0.5,"range":[0,1]},"antialias_threshold":{"value":0.002,"range":[0,0.1]},"noise_height":{"value":0.5,"range":[0,2]},"noise_scale":{"value":10,"range":[0,100]}}  ,
