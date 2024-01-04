fbe2c2171f1a6ef9977d7690dc7761eeae58f831
Abhimanyu sahani <sahanimannu844@gmail.com>
2024-01-04 13:38:33 +0530

    added first commit

---
+++ script.js
@@ -0,0 +1,77 @@
+// const scroll = new LocomotiveScroll({
+//   el: document.querySelector(".main"),
+//   smooth: true,
+// });
+
+
+
+  // Shery.mouseFollower( {
+
+  //   skew: true,
+  //   ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+  //   duration: 0.5,
+  // });
+
+
+
+
+
+
+// let tl = gsap.timeline()
+
+// tl.from(".nav a, .navb",{
+// y:20,
+// // duration:.3,
+// stagger:.5,
+// opacity:0,
+// scale:0,
+
+// })
+
+
+
+
+// Shery.imageEffect(".right-content img", {
+//   style: 7, //Select Style
+//   debug: true, // Debug Panel
+//   config: {
+//     /* Config made from debug panel */
+//   },
+//   preset: "./presets/wigglewobble.json",
+// });
+
+
+
+let img1 = document.querySelector(".page3 .right img")
+
+img1.addEventListener("mouseover", function(){
+  Shery.imageEffect(".page3 .right img", {
+    style: 2 /*OR 5 for different variant */,
+    debug: true,
+    gooey:true,
+    config: 
+      {"resolutionXY":{"value":100},"distortion":{"value":true},"mode":{"value":-3},"mousemove":{"value":0},"modeA":{"value":1},"modeN":{"value":0},"speed":{"value":1,"range":[-500,500],"rangep":[-10,10]},"frequency":{"value":50,"range":[-800,800],"rangep":[-50,50]},"angle":{"value":0.5,"range":[0,3.141592653589793]},"waveFactor":{"value":1.4,"range":[-3,3]},"color":{"value":10212607},"pixelStrength":{"value":3,"range":[-20,100],"rangep":[-20,20]},"quality":{"value":5,"range":[0,10]},"contrast":{"value":1,"range":[-25,25]},"brightness":{"value":1,"range":[-1,25]},"colorExposer":{"value":0.18,"range":[-5,5]},"strength":{"value":0.2,"range":[-40,40],"rangep":[-5,5]},"exposer":{"value":8,"range":[-100,100]},"zindex":{"value":-9996999,"range":[-9999999,9999999]},"aspect":{"value":0.9492483515729326},"ignoreShapeAspect":{"value":true},"shapePosition":{"value":{"x":0,"y":0}},"shapeScale":{"value":{"x":0.5,"y":0.5}},"shapeEdgeSoftness":{"value":0,"range":[0,0.5]},"shapeRadius":{"value":0,"range":[0,2]},"currentScroll":{"value":0},"scrollLerp":{"value":0.07},"gooey":{"value":true},"infiniteGooey":{"value":false},"growSize":{"value":4,"range":[1,15]},"durationOut":{"value":1,"range":[0.1,5]},"durationIn":{"value":1.5,"range":[0.1,5]},"displaceAmount":{"value":0.5},"masker":{"value":true},"maskVal":{"value":1,"range":[1,5]},"scrollType":{"value":0},"geoVertex":{"range":[1,64],"value":1},"noEffectGooey":{"value":true},"onMouse":{"value":0},"noise_speed":{"value":0.2,"range":[0,10]},"metaball":{"value":0.2,"range":[0,2]},"discard_threshold":{"value":0.5,"range":[0,1]},"antialias_threshold":{"value":0,"range":[0,0.1]},"noise_height":{"value":0.5,"range":[0,2]},"noise_scale":{"value":10,"range":[0,100]}}
+    ,
+
+  });
+})
+
+
+
+
+let imageEffect = document.querySelector(".page4 img")
+imageEffect.addEventListener("mouseover", function(){
+  Shery.imageEffect(".page4 img", {
+    style: 6, //Select Style
+    debug: true, // Debug Panel
+
+    gooey:true,
+    config:  {"noiseDetail":{"value":7.44,"range":[0,100]},"distortionAmount":{"value":2.98,"range":[0,10]},"scale":{"value":36.36,"range":[0,100]},"speed":{"value":0.79,"range":[0,1]},"zindex":{"value":-9996999,"range":[-9999999,9999999]},"aspect":{"value":0.6847925639670689},"ignoreShapeAspect":{"value":true},"shapePosition":{"value":{"x":0,"y":0}},"shapeScale":{"value":{"x":0.5,"y":0.5}},"shapeEdgeSoftness":{"value":0,"range":[0,0.5]},"shapeRadius":{"value":0,"range":[0,2]},"currentScroll":{"value":0},"scrollLerp":{"value":0.07},"gooey":{"value":true},"infiniteGooey":{"value":false},"growSize":{"value":4,"range":[1,15]},"durationOut":{"value":1,"range":[0.1,5]},"durationIn":{"value":1.5,"range":[0.1,5]},"displaceAmount":{"value":0.5},"masker":{"value":false},"maskVal":{"value":1,"range":[1,5]},"scrollType":{"value":0},"geoVertex":{"range":[1,64],"value":1},"noEffectGooey":{"value":true},"onMouse":{"value":0},"noise_speed":{"value":0.2,"range":[0,10]},"metaball":{"value":0.2,"range":[0,2]},"discard_threshold":{"value":0.5,"range":[0,1]},"antialias_threshold":{"value":0.002,"range":[0,0.1]},"noise_height":{"value":0.5,"range":[0,2]},"noise_scale":{"value":10,"range":[0,100]}}  ,
+    
+    preset: "./presets/wigglewobble.json",
+  });
+})
+
+
+
+  
